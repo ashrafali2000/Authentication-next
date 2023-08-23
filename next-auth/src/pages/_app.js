7fa1dd3249b80789929fe4703bd7e61b5f9c2baa
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-23 16:00:48 +0500

    updates

--- next-auth/src/pages/_app.js
+++ next-auth/src/pages/_app.js
@@ -1,11 +1,13 @@
 import { Header } from '@/components/header'
 import '@/styles/globals.css'
+import { useState } from 'react'
 
 export default function App({ Component, pageProps }) {
+  const [name , setName ] = useState("");
   return(
   <div>
     <Header></Header>
-<Component {...pageProps} />
+<Component {...pageProps} name = {name} setName = {setName} />
   </div> 
   )
 }
--- next-auth/src/pages/auth/signIn.js
+++ next-auth/src/pages/auth/signIn.js
@@ -1,13 +1,15 @@
 import Form from "@/components/form";
 import {signIn} from "next-auth/react"
 import { useRouter } from "next/router";
-export default function signUp(){
+export default function signUp({setName}){
     const router = useRouter();
 
     const onSingIn = async (email,password) => {
      try{
     const data = await signIn("credentials",{redirect:false,email,password})
+    console.log(data)
     if(data.ok) {
+        // setName(data.fullName);
         router.replace("/products");
     } 
     }
