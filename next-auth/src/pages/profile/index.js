e92ae50625d8294e8381aeda58589ffc914ea0ab
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-23 16:00:26 +0500

    profile added code

--- next-auth/src/pages/profile/index.js
+++ next-auth/src/pages/profile/index.js
@@ -1,7 +1,12 @@
-export default function Profile () {
+export default function Profile ({name}) {
     return (<div>
-        <div>
-            <h1>My Profile Page</h1>
+        <div style={{
+            display:"flex",
+            justifyContent:"center",
+            alignItems:"center"
+        }}>
+            <h1>{name} Profile Page</h1>
+            <img style={{borderRadius:"50%", width:150,height:150}} src= "https://media2.giphy.com/media/mD4JnP3TyUSmwkFwpo/200w.webp?cid=ecf05e47kht97k4htkajjcdmdw772ax0iagecbydm2xglrjg&ep=v1_gifs_search&rid=200w.webp&ct=g"></img>
         </div>
     </div>)
 }
