51e7205fadd2e897525f9ad7f5d4a57c65822dad
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-26 12:17:28 +0500

    create getServerSideProps function

--- next-auth/src/pages/profile/index.js
+++ next-auth/src/pages/profile/index.js
@@ -1,3 +1,5 @@
+import { getSession } from "next-auth/react"
+
 export default function Profile ({name}) {
     return (<div>
         <div style={{
@@ -9,4 +11,22 @@ export default function Profile ({name}) {
             <img style={{borderRadius:"50%", width:150,height:150}} src= "https://media2.giphy.com/media/mD4JnP3TyUSmwkFwpo/200w.webp?cid=ecf05e47kht97k4htkajjcdmdw772ax0iagecbydm2xglrjg&ep=v1_gifs_search&rid=200w.webp&ct=g"></img>
         </div>
     </div>)
+}
+
+export async function getServerSideProps({req}){
+    const session = await getSession({req});
+    if(!session){
+        return{
+            redirect:{
+                destination:"/auth/signIn",
+                perment:false
+            }
+        }
+    
+    }
+    return {
+        props:{
+            session
+        }
+    }
 }
