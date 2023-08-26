2c64236c5b41c6a7ce209699eac2b4d034ba07ce
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-26 12:54:05 +0500

    create btn components for login and logout

---
+++ next-auth/src/components/login_btn.js
@@ -0,0 +1,19 @@
+import { useSession, signOut } from "next-auth/react"
+import Link from "next/link"
+export default function Btn() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        {/* Signed in as {session.user.email} <br /> */}
+        <button onClick={() => signOut()} className="shadow-md py-2 px-6  bg-black rounded-full  text-white cursor-pointer font-semibold hover:opacity-70">Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      {/* Not signed in <br /> */}
+   <Link href={"/auth/signIn"}  className="shadow-md py-2 px-6  bg-indigo-600 rounded-full  text-white cursor-pointer font-semibold hover:opacity-70"> Sign in</Link>  
+    </>
+  )
+}
